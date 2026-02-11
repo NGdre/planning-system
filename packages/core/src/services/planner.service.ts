5d55bb1fc4439904997d303f182d2fc7778e0930
NGdre <100stepstosuccess@gmail.com>
2026-02-11 15:42:55 +0300

    refactor: add hasOverlappingTimeBlocks
    this logic is needed for both scheduling and rescheduling

--- packages/core/src/services/planner.service.ts
+++ packages/core/src/services/planner.service.ts
@@ -14,19 +14,13 @@ export class PlannerService {
   async schedule(taskId: string, startTime: Date, endTime: Date): Promise<boolean> {
     if (!this.isFutureTimeBlock(startTime, endTime) || !this.isSonnerThanMonth) return false
 
+    if (await this.hasOverlappingTimeBlocks(startTime, endTime)) return false
+
     const desiredTimeBlock = TimeBlockEntity.create(this.idGenerator, {
       taskId,
       startTime,
       endTime,
     })
-
-    const busyTimeBlocks = await this.findClosestTimeBlocks(startTime, endTime)
-
-    for (const currTimeBlockDate of busyTimeBlocks) {
-      const busyTimeBlock = TimeBlockEntity.restore(currTimeBlockDate)
-
-      if (desiredTimeBlock.overlaps(busyTimeBlock)) return false
-    }
 
     await this.timeBlockRepository.save(desiredTimeBlock.toData())
 
@@ -71,5 +65,37 @@ export class PlannerService {
     const monthAhead = new Date(currTime.getTime() + oneMonth)
 
     return endTime <= monthAhead
+  }
+
+  /**
+   * Checks all potential overlapping time blocks with given one
+   *
+   * @param startTime - Start of given time block
+   * @param endTime - End of given time block
+   * @param excludeId - Excludes id of time block. It is useful when there's
+   * a time block in repository that have to be rescheduled
+   * @returns true if there's overlapping time blocks, false otherwise
+   */
+  private async hasOverlappingTimeBlocks(
+    startTime: Date,
+    endTime: Date,
+    excludeId?: string
+  ): Promise<boolean> {
+    const nearbyBlocks = await this.findClosestTimeBlocks(startTime, endTime)
+
+    const desiredBlock = { startTime, endTime }
+
+    for (const blockData of nearbyBlocks) {
+      if (excludeId && blockData.id === excludeId) {
+        continue
+      }
+
+      const busyBlock = TimeBlockEntity.restore(blockData)
+      if (busyBlock.overlaps(desiredBlock)) {
+        return true
+      }
+    }
+
+    return false
   }
 }
