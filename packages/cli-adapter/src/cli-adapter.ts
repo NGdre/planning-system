03329ba715326b8232f928524337a4747dda0a10
NGdre <100stepstosuccess@gmail.com>
2026-02-12 15:52:18 +0300

    feat: add time block use cases in cli adapter

--- packages/cli-adapter/src/cli-adapter.ts
+++ packages/cli-adapter/src/cli-adapter.ts
@@ -4,23 +4,31 @@ import {
   ListTasksUseCase,
   GetSpecificTaskUseCase,
   UserActionsService,
+  ScheduleTimeBlockForTask,
+  PlannerService,
+  ShowAvailableSlotsForDayUseCase,
 } from '@planning-system/core'
 import { KnexTaskRepository } from './persistence/repositories/task.repo.js'
 import { v4 as uuid } from 'uuid'
 import { DatabaseConnection } from './persistence/db.js'
+import { KnexTimeBlockRepository } from './persistence/repositories/time-block.repo.js'
 
 export class CLIAdapter {
   constructor(
     private readonly taskRepo: KnexTaskRepository,
-    private readonly userActionsService: UserActionsService
+    private readonly timeBlockRepo: KnexTimeBlockRepository,
+    private readonly userActionsService: UserActionsService,
+    private readonly plannerService: PlannerService
   ) {}
 
   static async create() {
     const db = await DatabaseConnection.getConnection()
     const taskRepo = new KnexTaskRepository(db)
+    const timeBlockRepo = new KnexTimeBlockRepository(db)
     const userActionsService = new UserActionsService()
+    const plannerService = new PlannerService(timeBlockRepo, uuid)
 
-    return new CLIAdapter(taskRepo, userActionsService)
+    return new CLIAdapter(taskRepo, timeBlockRepo, userActionsService, plannerService)
   }
 
   async createTask(input: CreateTaskInput) {
@@ -33,5 +41,17 @@ export class CLIAdapter {
 
   async getSpecificTask(id: string) {
     return await new GetSpecificTaskUseCase(this.taskRepo, this.userActionsService).execute(id)
+  }
+
+  async schedule(taskId: string, startTime: string, endTime: string) {
+    return await new ScheduleTimeBlockForTask(this.plannerService).execute(
+      taskId,
+      startTime,
+      endTime
+    )
+  }
+
+  async showAvailableSlots(day: string) {
+    return await new ShowAvailableSlotsForDayUseCase(this.plannerService).execute(day)
   }
 }
