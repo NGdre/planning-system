c2190cab8f496e8de468b214387047bf5d773f73
NGdre <100stepstosuccess@gmail.com>
2026-03-02 19:55:22 +0300

    feat: create time tracking service

---
+++ packages/core/src/services/time-tracking.service.ts
@@ -0,0 +1,152 @@
+import { SessionDTO, SessionEntity } from '../entities/session.entity.js'
+import { IdGenerator, SessionRepository } from '../ports/repository.port.js'
+import { Result, VoidResult } from '../types.js'
+
+export class TimeTrackingService {
+  constructor(
+    private readonly sessionRepository: SessionRepository,
+    private readonly idGenerator: IdGenerator
+  ) {}
+
+  async startSession(sessionParams: {
+    taskId: string | null
+    timeBlockId: string | null
+  }): Promise<VoidResult> {
+    try {
+      const activeSession = await this.sessionRepository.findActive()
+
+      if (activeSession) {
+        return {
+          success: false,
+          error: 'Cannot start a new session while another is active',
+        }
+      }
+
+      const session = SessionEntity.create(this.idGenerator, sessionParams)
+      await this.sessionRepository.save(session.toData())
+
+      return { success: true }
+    } catch (error) {
+      console.error('Failed to start session:', error)
+      return {
+        success: false,
+        error: error instanceof Error ? error.message : 'Unknown error',
+      }
+    }
+  }
+
+  async findAllSessions(): Promise<Result<SessionDTO[]>> {
+    try {
+      const sessions = await this.sessionRepository.findAll()
+
+      return {
+        success: true,
+        value: sessions,
+      }
+    } catch (error) {
+      console.error('Failed to find sessions:', error)
+
+      return {
+        success: false,
+        error: error instanceof Error ? error.message : 'Unknown error',
+      }
+    }
+  }
+
+  async pauseSession(): Promise<VoidResult> {
+    try {
+      const sessionDTO = await this.findActiveSession()
+
+      if (!sessionDTO.success) return sessionDTO
+
+      const session = SessionEntity.restore(sessionDTO.value)
+
+      session.pause()
+
+      await this.sessionRepository.save(session.toData())
+
+      return {
+        success: true,
+      }
+    } catch (error) {
+      console.error('Failed to pause session:', error)
+
+      return {
+        success: false,
+        error: error instanceof Error ? error.message : 'Unknown error',
+      }
+    }
+  }
+
+  async resumeSession(): Promise<VoidResult> {
+    try {
+      const sessionDTO = await this.findActiveSession()
+
+      if (!sessionDTO.success) return sessionDTO
+
+      const session = SessionEntity.restore(sessionDTO.value)
+
+      session.resume()
+
+      await this.sessionRepository.save(session.toData())
+
+      return {
+        success: true,
+      }
+    } catch (error) {
+      console.error('Failed to resume session:', error)
+
+      return {
+        success: false,
+        error: error instanceof Error ? error.message : 'Unknown error',
+      }
+    }
+  }
+
+  async stopSession(): Promise<VoidResult> {
+    try {
+      const sessionDTO = await this.findActiveSession()
+
+      if (!sessionDTO.success) return sessionDTO
+
+      const session = SessionEntity.restore(sessionDTO.value)
+
+      session.stop()
+
+      await this.sessionRepository.save(session.toData())
+
+      return {
+        success: true,
+      }
+    } catch (error) {
+      console.error('Failed to stop session:', error)
+
+      return {
+        success: false,
+        error: error instanceof Error ? error.message : 'Unknown error',
+      }
+    }
+  }
+
+  private async findActiveSession(): Promise<Result<SessionDTO>> {
+    try {
+      const sessionDTO = await this.sessionRepository.findActive()
+
+      if (!sessionDTO)
+        return {
+          success: false,
+          error: 'Failed to find active session',
+        }
+
+      return {
+        success: true,
+        value: sessionDTO,
+      }
+    } catch (error) {
+      return {
+        success: false,
+        error: error instanceof Error ? error.message : 'Unknown error',
+      }
+    }
+  }
+}
