91dff82ff9a859b0b95bb15487b3021e42241260
NGdre <100stepstosuccess@gmail.com>
2026-03-13 16:38:58 +0300

    feat: add navigation to new task menu

--- apps/cli/src/components/menu/NewTaskMenu.tsx
+++ apps/cli/src/components/menu/NewTaskMenu.tsx
@@ -1,29 +1,10 @@
-import { useRef, useState } from 'react'
-import SelectInput from 'ink-select-input'
-import { Text } from 'ink'
-import { TextInputContainer } from '../TextInputContainer.js'
 import { CreateTaskInput } from '@planning-system/core'
+import { Text } from 'ink'
+import SelectInput from 'ink-select-input'
+import { useRef, useState } from 'react'
 import { cliAdapter } from '../../cli.js'
 import { useNavigation } from '../navigation/NavigationContext.js'
-
-const newTaskMenu = [
-  {
-    label: 'Название задачи',
-    value: 'TASK_TITLE',
-    fieldName: 'title',
-    textInputLabel: 'Введите название задачи',
-  },
-  {
-    label: 'Описание задачи',
-    value: 'TASK_DESCRIPTION',
-    fieldName: 'description',
-    textInputLabel: 'Введите описание задачи',
-  },
-  {
-    label: 'Готово!',
-    value: 'DONE',
-  },
-]
+import { TextInputContainer } from '../TextInputContainer.js'
 
 export default function NewTaskMenu() {
   const newTaskFields = useRef<CreateTaskInput | null>(null)
@@ -50,8 +31,42 @@ export default function NewTaskMenu() {
       return
     }
 
+    if (item.value === 'HOME') push({ name: 'MainMenu' })
+
     setMenuItem(item)
   }
+
+  // TODO: more sophisticated validation is needed
+  const isValidTask = (() => {
+    return newTaskFields.current?.title !== undefined
+  })()
+
+  const newTaskMenu = [
+    {
+      label: 'Название задачи',
+      value: 'TASK_TITLE',
+      fieldName: 'title',
+      textInputLabel: 'Введите название задачи',
+    },
+    {
+      label: 'Описание задачи',
+      value: 'TASK_DESCRIPTION',
+      fieldName: 'description',
+      textInputLabel: 'Введите описание задачи',
+    },
+    ...(isValidTask
+      ? [
+          {
+            label: 'Готово!',
+            value: 'DONE',
+          },
+        ]
+      : []),
+    {
+      label: 'В главное меню',
+      value: 'HOME',
+    },
+  ]
 
   return (
     <>
